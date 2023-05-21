58fbb239e4f24fb84d19c05745fcfced293f6ca6
aflanders7 <114197067+aflanders7@users.noreply.github.com>
2023-05-21 16:50:54 -0400

    Update Home.svelte

--- frontend/src/Home.svelte
+++ frontend/src/Home.svelte
@@ -27,16 +27,15 @@
   
   
   
-      <h2 class="intro">Welcome to a safe space.</h2>
-    <h2>Ask mental health related questions or express your feelings:</h2>
+      <h2 class="intro">Welcome to a safe space to reflect on your mental health.</h2>
   
-    <p>INSERT CHATBOT HERE</p>
+  
   
   </div>
   
   <footer><p>Disclaimer: This website provides information for informational purposes only. This website does not provide any 
     medical or professional advice. This chatbot does not replace a healthcare professional or act as a substitute for 
     professional mental health, mental advice, diagnosis or treatment.</p>
-  &copy; 2023 BeaverHacks IDK***
+  &copy; 2023 Audrey Flanders and Lorenzo Balderrama
   </footer>
   
