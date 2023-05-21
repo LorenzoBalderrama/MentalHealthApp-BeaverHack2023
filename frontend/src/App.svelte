323a7ee998deccc7ed54587428af09787300c8a7
aflanders7 <114197067+aflanders7@users.noreply.github.com>
2023-05-21 16:50:35 -0400

    Update App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,10 +4,12 @@
   import breath from './breath.svelte';
   import about from './about.svelte';
   import reflect from './reflect.svelte';
+  import chatbot from './chatbot.svelte';
 </script>
 
 <nav class ="main">
   <a href="/#/">Home</a>
+  <a href="/#/chatbot">Chat</a>
   <a href="/#/breath">Breath</a>
   <a href="/#/reflect">Reflect</a>
   <a href="/#/about">About</a>
@@ -15,7 +17,8 @@
 
 <Router routes={{
   '/': Home,
+  '/chatbot': chatbot,
   '/breath': breath,
   '/reflect': reflect,
-  '/about': about
+  '/about': about,
 }} />
